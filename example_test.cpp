130591f7930e4cca8aabb07183b8f5a8b1ba6d19
vanyabeat <vanyabeat@yandex.ru>
2022-02-07 12:59:27 +0300

    ADD: naive DmitryL tests

--- example_test.cpp
+++ example_test.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
-
+#include <string>
 TEST(GoogleTestCi, Foo) {
   ASSERT_TRUE(true);
+}
+TEST(ExampleTest, DmitriiLeonidovich){
+    std::string dml = "DmitriiLeonidovich";
+    const char * dml_c = "DmitriiLeonidovich";
+    ASSERT_STREQ(dml.c_str(), dml_c);
 }
